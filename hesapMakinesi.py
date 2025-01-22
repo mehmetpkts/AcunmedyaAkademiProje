8272ccc2886565f8c41dbe0c3ea5812adb303f05
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2025-01-22 23:11:00 +0300

    hesap makinesi kodlari yazildi!

--- hesapMakinesi.py
+++ hesapMakinesi.py
@@ -1,3 +1,17 @@
-print("Hello world!")
+# hesap makinesi:
 
-print("ikici commit")
+a = int(input("Lutfen ilk sayiyi giriniz:"))
+b = int(input("Lutfen ikinci sayiyi giriniz:"))
+
+c = input("Lutfen islem gir:")
+
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
+else:
+    print("Yanlis bir deger girdin!")
